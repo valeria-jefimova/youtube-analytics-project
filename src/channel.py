927ec1a481addbe712de8861fd71808833aa774c
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-14 22:02:29 +0200

    fix channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
+import json
+import os
+
+from helper.youtube_api_manual import channel
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
