679222923b15cd9a10f07e79302c25aa455f7345
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-14 22:37:22 +0200

    fix 2иchannel.py

--- src/channel.py
+++ src/channel.py
@@ -11,7 +11,7 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
