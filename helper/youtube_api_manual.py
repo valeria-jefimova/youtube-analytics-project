5897c5279f07cd05810c0955f73c3a0c9a41451d
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-03-14 23:16:38 +0200

    fix 3 channel.py

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -16,7 +16,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 def printj(dict_to_print: dict) -> None:
     """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+    printj(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
 '''
--- src/channel.py
+++ src/channel.py
@@ -5,15 +5,16 @@ from distutils.command.build import build
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
+    api_key = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
